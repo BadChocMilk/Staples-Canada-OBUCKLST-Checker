c4e75368440973e83178c6e38dffc41b3b371343
Robin <badchocmilk@gmail.com>
2025-01-04 14:57:51 -0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -12,11 +12,28 @@ function checkOpenBox() {
         const workbook = XLSX.read(data, {type: "array"});
         const worksheet = workbook.Sheets[workbook.SheetNames[0]];
         const cvsData = XLSX.utils.sheet_to_csv(worksheet);
-        console.log(workbook);
+        // A for SKU, C for item Desc., F for class, J for openbox quantity
     }
 
     fileReader.readAsArrayBuffer(xlf1);
     console.log("Hello from the console!");
+    worksheetToArray(worksheet);
+    
+}
+
+function worksheetToArray(worksheet) {
+
+    let temp = 0;
+    let column = "A" + temp;
+    try{
+        while(true){
+            worksheet[column]["v"];
+            temp++;
+            column = "A" + temp;
+        }
+    }catch{
+        console.log(temp);
+    }
     
 }
 
