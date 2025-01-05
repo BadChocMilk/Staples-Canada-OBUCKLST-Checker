ac1a79c87e0636d83b3d6c75b30c06bf4c8e76f0
Robin <badchocmilk@gmail.com>
2025-01-04 19:15:52 -0500

    update openbox.js

--- js/openbox.js
+++ js/openbox.js
@@ -1,5 +1,6 @@
 function checkOpenBox() {
-
+    let oldArray;
+    let newArray;
     // most of this function was taken from the online guide https://medium.com/@sudipb001/how-to-convert-an-excel-file-to-a-csv-file-using-javascript-4e688e167641. Thank you Sudip!
     if (document.getElementById("xlf1").files.length === 0){
         alert("No file is selected!")
@@ -9,32 +10,39 @@ function checkOpenBox() {
 
     const xlf1 = document.getElementById("xlf1").files[0];
     const fileReader = new FileReader();
+   
 
     fileReader.onload = function (e){
         const data = new Uint8Array(e.target.result);
         const workbook = XLSX.read(data, {type: "array"});
         const worksheet = workbook.Sheets[workbook.SheetNames[0]];
         const cvsData = XLSX.utils.sheet_to_csv(worksheet);
-        let oldArray = worksheetToArray(worksheet);
+        oldArray = worksheetToArray(worksheet);
         oldArray = sortArray(oldArray);
+        const xlf2 = document.getElementById("xlf2").files[0];
+        const fileReader2 = new FileReader();
+      
+    
+        fileReader2.onload = function (e){
+            const data = new Uint8Array(e.target.result);
+            const workbook = XLSX.read(data, {type: "array"});
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+            const cvsData = XLSX.utils.sheet_to_csv(worksheet);
+            console.log(worksheet);
+            newArray = worksheetToArray(worksheet);
+            newArray = sortArray(newArray);
+            let negativeArray = checkForNegatives(worksheet);
+            compareArrays(oldArray, newArray);
+        }
+        fileReader2.readAsArrayBuffer(xlf2);
     }
-
-    const xlf2 = document.getElementById("xlf2").files[0];
-    fileReader = new FileReader();
-
-    fileReader.onload = function (e){
-        const data = new Uint8Array(e.target.result);
-        const workbook = XLSX.read(data, {type: "array"});
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        const cvsData = XLSX.utils.sheet_to_csv(worksheet);
-        let newArray = worksheetToArray(worksheet);
-        newArray = sortArray(newArray);
-    }
-
+    fileReader.readAsArrayBuffer(xlf1);
+    
     console.log("Hello from the console!");
 }
 
 function worksheetToArray(worksheet) {
+
 
     let workArray = [];
 
@@ -51,8 +59,6 @@ function worksheetToArray(worksheet) {
         // because there are column labels, subtract one.
         numRows--;
     }
-    // number of openbox
-    let numOB = 0
     for(let i = 2; i <= numRows; i++){
         // A for SKU, C for item Desc., F for class, J for openbox quantity
         let sku = "A" + i;
@@ -66,8 +72,6 @@ function worksheetToArray(worksheet) {
         }
         // adds them all to an array consisting of ONLY openbox products.
         workArray.push([worksheet[storeClass]['w'], worksheet[sku]['w'], worksheet[desc]['w'], worksheet[openBox]['w']]);
-        // once the product is added to the array, we want the number of openbox to go up!
-        numOB++;
     }
     return workArray;
 }
@@ -81,4 +85,59 @@ function sortArray(array){
 
 function compareArrays(oldArray, newArray){
 
+    let final = "SKU\tDescription\tOpenbox\nOPENBOX TO COME OFF\n"
+    console.log(oldArray);
+
+    // finding the openbox SKUs that need to come off
+    console.log(oldArray.length);
+    console.log(newArray.length);
+    for(let i = 0; i < oldArray.length; i++){
+        for(let j = 0; j < newArray.length; j++){
+            if(oldArray[i][1] != newArray[j][1]){
+                //console.log(oldArray[i][1] + "\t" + newArray[j][1])
+                continue;
+            }
+            else{
+                console.log("equal found!");
+                break;
+            }
+            final = final + oldArray[i][1] + oldArray[i][2] + oldArray[i][3];
+        }
+    }
+
+    console.log(final);
+}
+
+function checkForNegatives(worksheet){
+    // checks to see if any of the openbox products have negatives to be corrected. Very similar to worksheetToArray
+
+    let workArray = [];
+
+    let numRows = 1;
+    let column = "A" + numRows;
+    // try catch counts every entry in the list.
+    try{
+        while(true){
+            worksheet[column]["v"];
+            numRows++;
+            column = "A" + numRows;
+        }
+    }catch{
+        // because there are column labels, subtract one.
+        numRows--;
+    }
+    for(let i = 2; i <= numRows; i++){
+        // A for SKU, C for item Desc., J for openbox quantity
+        let sku = "A" + i;
+        let desc = "C" + i;
+        let openBox = "J" + i;
+
+        // checks to see if there are any negative openbox products.
+        if(worksheet[openBox]['v'] > -1){
+            continue;
+        }
+        // adds them all to an array consisting of ONLY negative openbox products.
+        workArray.push([worksheet[sku]['w'], worksheet[desc]['w'], worksheet[openBox]['w']]);
+    }
+    return workArray;
 }
