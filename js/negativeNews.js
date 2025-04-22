f0aec8c691fe276ce5fb5e0e292bd8fed3500fcf
BadChocMilk <robin.c.resendes@gmail.com>
2025-04-22 14:16:25 -0400

    update index.html and negativeNews.js

--- js/negativeNews.js
+++ js/negativeNews.js
@@ -1,5 +1,14 @@
 function negativeNewsButton(){
 
+    // checks if the file is there
+    if (document.getElementById("xlf2").files.length === 0){
+        alert("To get a negative new list, only current needs a file.")
+        return
+    }
+
+    //clear text area
+    clearTextArea();
+    negativeNews();
 }
 
 function negativeNews(){
@@ -14,9 +23,9 @@ function negativeNews(){
         const workbook = XLSX.read(data, {type: "array"});
         const worksheet = workbook.Sheets[workbook.SheetNames[0]];
         const cvsData = XLSX.utils.sheet_to_csv(worksheet);
-        obList = worksheetToArray(worksheet);
+        obList = worksheetToNegativeNewArray(worksheet);
         obList = sortArray(obList);
-        final = arrayToOBString(obList);
+        final = arrayToNegativeNewString(obList);
     }
     fileReader.readAsArrayBuffer(xlf2)
 }
@@ -38,7 +47,7 @@ function worksheetToNegativeNewArray(worksheet){
         numRows--;
     }
     for(let i = 2; i <= numRows; i++){
-        // A for SKU, C for item Desc., F for class, G for verify quantity
+        // the numbers here align with what the number cell in the spreadsheet would be.
         let sku = "A" + i;
         let desc = "C" + i;
         let storeClass = "F" + i;
@@ -51,8 +60,24 @@ function worksheetToNegativeNewArray(worksheet){
         if(worksheet[negativeNew]['v'] >= 0){
             continue;
         }
-        // adds them all to an array consisting of ONLY verify products.
-        workArray.push([worksheet[storeClass]['w'], skuFormatter(worksheet[sku]['w']), descriptionFormatter(worksheet[desc]['w']), worksheet[verify]['w']]);
+        // adds them all to an array consisting of negative new products, along with where the source of the negative new would be.
+        workArray.push([worksheet[storeClass]['w'], skuFormatter(worksheet[sku]['w']), descriptionFormatter(worksheet[desc]['w']), worksheet[negativeNew]['w'], worksheet[demo]['w'], worksheet[verify]['w'], worksheet[openBox]['w']]);
     }
     return workArray;
+}
+
+function arrayToNegativeNewString(array){
+    // this will turn the array into a string to be displayed in the text area.
+    let final ="SKU\t\tDescription\t\t\t\tNegativeNew\t   Demo\t\tVerify\t\tOpenBox\n";
+    let count = 0;
+    
+    // random spaces and \t's make for a nicer looking list when it gets displayed.
+    for(let i = 0; i < array.length; i++){
+            final = final + array[i][1] + "\t" + array[i][2]+ "\t\t    " + array[i][3] + "\t\t     " + array[i][4] + "\t\t   " + array[i][5] + "\t\t   " + array[i][6] + "\n";
+            count++;
+    }
+    final = final + "\nTotal: " + count + "\n\n";
+
+    addToTextArea(final);
+    return final;
 }
